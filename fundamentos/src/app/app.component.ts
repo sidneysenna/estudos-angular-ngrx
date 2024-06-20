f2470c7a928ea2bf0219332b8a5225dec254a752
sidney senna <sidney.senna@gmail.com>
2024-06-20 07:58:12 -0300

    feat: criacao do projeto e property bind

---
+++ fundamentos/src/app/app.component.ts
@@ -0,0 +1,24 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrl: './app.component.scss',
+})
+export class AppComponent {
+  title = 'fundamentos';
+
+  inputText = 'inputinho';
+  inputDisabled = true;
+
+  toogleInput() {
+    this.inputDisabled = !this.inputDisabled;
+  }
+
+  logInputText() {
+    console.log(
+      'O valor nao eh alterado pq esta em uma unica direcao...',
+      this.inputText
+    );
+  }
+}
